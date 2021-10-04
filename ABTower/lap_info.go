3472be6190e25b4010ddd05da95968da5d525b01
dubinin-dmytro <dubinin.dmytro@airslate.com>
2021-10-04 20:26:37 +0300

    Update LapInfo struct

--- ABTower/lap_info.go
+++ ABTower/lap_info.go
@@ -1,4 +1,4 @@
-package ABTower
+package main
 
 type LapInfoResponse struct {
 	Success bool    `json:"success"`
@@ -6,181 +6,96 @@ type LapInfoResponse struct {
 	Data    LapInfo `json:"data"`
 }
 
+type PValueBranch struct {
+	Registered struct {
+		WinRate float64 `json:"win-rate"`
+		PValue  float64 `json:"p-value"`
+	} `json:"registered"`
+	GoToEditor struct {
+		WinRate float64 `json:"win-rate"`
+		PValue  float64 `json:"p-value"`
+	} `json:"go_to_editor"`
+	Payment struct {
+		WinRate float64 `json:"win-rate"`
+		PValue  float64 `json:"p-value"`
+	} `json:"payment"`
+}
+
+type EventListBranch struct {
+	Total struct {
+		Number float64 `json:"number"`
+	} `json:"total"`
+	Registered struct {
+		Number     float64 `json:"number"`
+		Conversion float64 `json:"conversion"`
+	} `json:"registered"`
+	ChoiceShow struct {
+		Number     float64 `json:"number"`
+		Conversion float64 `json:"conversion"`
+	} `json:"choice_show"`
+	GoToEditor struct {
+		Number     float64 `json:"number"`
+		Conversion float64 `json:"conversion"`
+	} `json:"go_to_editor"`
+	Payment struct {
+		Number     float64 `json:"number"`
+		Conversion float64 `json:"conversion"`
+	} `json:"payment"`
+	AmountToTotal struct {
+		Number float64 `json:"number"`
+	} `json:"amount_to_total"`
+	SupportTicket struct {
+		Number float64 `json:"number"`
+	} `json:"support_ticket"`
+	SubscriptionCancel struct {
+		Number     float64 `json:"number"`
+		Conversion float64 `json:"conversion"`
+	} `json:"subscription_cancel"`
+	Refund struct {
+		Number     float64 `json:"number"`
+		Conversion float64 `json:"conversion"`
+	} `json:"refund"`
+	WillPay struct {
+		Number     float64 `json:"number"`
+		Conversion float64 `json:"conversion"`
+	} `json:"will_pay"`
+	WillPayAmount struct {
+		Number float64 `json:"number"`
+	} `json:"will_pay_amount"`
+	WillPayAmountToTotal struct {
+		Number float64 `json:"number"`
+	} `json:"will_pay_amount_to_total"`
+	CanceledBeforePay struct {
+		Number     float64 `json:"number"`
+		Conversion float64 `json:"conversion"`
+	} `json:"canceled_before_pay"`
+	CanceledBeforePayAmount struct {
+		Number float64 `json:"number"`
+	} `json:"canceled_before_pay_amount"`
+	PartialRefund struct {
+		Number     float64 `json:"number"`
+		Conversion float64 `json:"conversion"`
+	} `json:"partial_refund"`
+	SupportChat struct {
+		Number float64 `json:"number"`
+	} `json:"support_chat"`
+	SupportCall struct {
+		Number float64 `json:"number"`
+	} `json:"support_call"`
+	PaymentAmount struct {
+		Number float64 `json:"number"`
+	} `json:"payment_amount"`
+	CcGiven struct {
+		Number float64 `json:"number"`
+	} `json:"cc_given"`
+	CcGivenAmount struct {
+		Number float64 `json:"number"`
+	} `json:"cc_given_amount"`
+}
+
 type LapInfo struct {
-	EventList struct {
-		Original struct {
-			Total struct {
-				Number float64 `json:"number"`
-			} `json:"total"`
-			Registered struct {
-				Number     float64 `json:"number"`
-				Conversion float64 `json:"conversion"`
-			} `json:"registered"`
-			ChoiceShow struct {
-				Number     float64 `json:"number"`
-				Conversion float64 `json:"conversion"`
-			} `json:"choice_show"`
-			GoToEditor struct {
-				Number     float64 `json:"number"`
-				Conversion float64 `json:"conversion"`
-			} `json:"go_to_editor"`
-			Payment struct {
-				Number     float64 `json:"number"`
-				Conversion float64 `json:"conversion"`
-			} `json:"payment"`
-			AmountToTotal struct {
-				Number float64 `json:"number"`
-			} `json:"amount_to_total"`
-			SupportTicket struct {
-				Number float64 `json:"number"`
-			} `json:"support_ticket"`
-			SubscriptionCancel struct {
-				Number     float64 `json:"number"`
-				Conversion float64 `json:"conversion"`
-			} `json:"subscription_cancel"`
-			Refund struct {
-				Number     float64 `json:"number"`
-				Conversion float64 `json:"conversion"`
-			} `json:"refund"`
-			WillPay struct {
-				Number     float64 `json:"number"`
-				Conversion float64 `json:"conversion"`
-			} `json:"will_pay"`
-			WillPayAmount struct {
-				Number float64 `json:"number"`
-			} `json:"will_pay_amount"`
-			WillPayAmountToTotal struct {
-				Number float64 `json:"number"`
-			} `json:"will_pay_amount_to_total"`
-			CanceledBeforePay struct {
-				Number     float64 `json:"number"`
-				Conversion float64 `json:"conversion"`
-			} `json:"canceled_before_pay"`
-			CanceledBeforePayAmount struct {
-				Number float64 `json:"number"`
-			} `json:"canceled_before_pay_amount"`
-			PartialRefund struct {
-				Number     float64 `json:"number"`
-				Conversion float64 `json:"conversion"`
-			} `json:"partial_refund"`
-			SupportChat struct {
-				Number float64 `json:"number"`
-			} `json:"support_chat"`
-			SupportCall struct {
-				Number float64 `json:"number"`
-			} `json:"support_call"`
-			PaymentAmount struct {
-				Number float64 `json:"number"`
-			} `json:"payment_amount"`
-			CcGiven struct {
-				Number float64 `json:"number"`
-			} `json:"cc_given"`
-			CcGivenAmount struct {
-				Number float64 `json:"number"`
-			} `json:"cc_given_amount"`
-		} `json:"original"`
-		MultiCurl struct {
-			Total struct {
-				Number float64 `json:"number"`
-			} `json:"total"`
-			Registered struct {
-				Number     float64 `json:"number"`
-				Conversion float64 `json:"conversion"`
-			} `json:"registered"`
-			ChoiceShow struct {
-				Number     float64 `json:"number"`
-				Conversion float64 `json:"conversion"`
-			} `json:"choice_show"`
-			GoToEditor struct {
-				Number     float64 `json:"number"`
-				Conversion float64 `json:"conversion"`
-			} `json:"go_to_editor"`
-			Payment struct {
-				Number     float64 `json:"number"`
-				Conversion float64 `json:"conversion"`
-			} `json:"payment"`
-			AmountToTotal struct {
-				Number float64 `json:"number"`
-			} `json:"amount_to_total"`
-			SupportTicket struct {
-				Number float64 `json:"number"`
-			} `json:"support_ticket"`
-			SubscriptionCancel struct {
-				Number     float64 `json:"number"`
-				Conversion float64 `json:"conversion"`
-			} `json:"subscription_cancel"`
-			Refund struct {
-				Number     float64 `json:"number"`
-				Conversion float64 `json:"conversion"`
-			} `json:"refund"`
-			WillPay struct {
-				Number     float64 `json:"number"`
-				Conversion float64 `json:"conversion"`
-			} `json:"will_pay"`
-			WillPayAmount struct {
-				Number float64 `json:"number"`
-			} `json:"will_pay_amount"`
-			WillPayAmountToTotal struct {
-				Number float64 `json:"number"`
-			} `json:"will_pay_amount_to_total"`
-			CanceledBeforePay struct {
-				Number     float64 `json:"number"`
-				Conversion float64 `json:"conversion"`
-			} `json:"canceled_before_pay"`
-			CanceledBeforePayAmount struct {
-				Number float64 `json:"number"`
-			} `json:"canceled_before_pay_amount"`
-			PartialRefund struct {
-				Number     float64 `json:"number"`
-				Conversion float64 `json:"conversion"`
-			} `json:"partial_refund"`
-			SupportChat struct {
-				Number float64 `json:"number"`
-			} `json:"support_chat"`
-			SupportCall struct {
-				Number float64 `json:"number"`
-			} `json:"support_call"`
-			PaymentAmount struct {
-				Number float64 `json:"number"`
-			} `json:"payment_amount"`
-			CcGiven struct {
-				Number float64 `json:"number"`
-			} `json:"cc_given"`
-			CcGivenAmount struct {
-				Number float64 `json:"number"`
-			} `json:"cc_given_amount"`
-		} `json:"multi-curl"`
-	} `json:"eventList"`
-	PValues struct {
-		Original struct {
-			Registered struct {
-				WinRate float64 `json:"win-rate"`
-				PValue  float64 `json:"p-value"`
-			} `json:"registered"`
-			GoToEditor struct {
-				WinRate float64 `json:"win-rate"`
-				PValue  float64 `json:"p-value"`
-			} `json:"go_to_editor"`
-			Payment struct {
-				WinRate float64 `json:"win-rate"`
-				PValue  float64 `json:"p-value"`
-			} `json:"payment"`
-		} `json:"original"`
-		MultiCurl struct {
-			Registered struct {
-				WinRate float64 `json:"win-rate"`
-				PValue  float64 `json:"p-value"`
-			} `json:"registered"`
-			GoToEditor struct {
-				WinRate float64 `json:"win-rate"`
-				PValue  float64 `json:"p-value"`
-			} `json:"go_to_editor"`
-			Payment struct {
-				WinRate float64 `json:"win-rate"`
-				PValue  float64 `json:"p-value"`
-			} `json:"payment"`
-		} `json:"multi-curl"`
-	} `json:"pValues"`
+	EventList map[string]EventListBranch `json:"eventList"`
+	PValues   map[string]PValueBranch    `json:"pValues"`
 }
 
 func (l LapInfoResponse) isSuccess() bool {
